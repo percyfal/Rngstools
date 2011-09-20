dffba8484381ba2aa904b5b190b48b0392bac3fc
Per Unneberg <per.unneberg@ki.se>
2011-09-20 16:39:12 +0200

    Refactored code from S3 to S4

--- R/diBayes.R
+++ R/diBayes.R
@@ -4,117 +4,168 @@
                     "ti", "tv", "ti", "tv", "tv", "ti", "tv", "tv", "ti", "tv",
                     "tv", "ti", "tv", "ti", "tv", "ti", "tv", "tv", "ti", "tv"))
 .iub <- c("A", "C", "G", "T", "M", "R", "W", "S", "Y", "K")
+.titv.df <- matrix(.titv, ncol=4)
+colnames(.titv.df) <- .iub[1:4]
+rownames(.titv.df) <- .iub
 
-## TODO: Make S4 generic function on a RangedData object
-plotQCdiBayes <-
-function(db, steps=c(1,5,100), breaks=c(50, 100, 10000), ...) {
-    if (length(steps) != length(breaks))
-        stop("length(steps) != length(breaks)")
-    cr <- range(db$coverage)
-    s <- c(hist(db$coverage[db$coverage >= min(cr) & db$coverage < breaks[1]], plot=FALSE, breaks=(breaks[1]-min(cr))/steps[1])$breaks,
-           hist(db$coverage[db$coverage >= breaks[1] & db$coverage < breaks[2]], plot=FALSE, breaks=(breaks[2]-breaks[1])/steps[2])$breaks,
-           hist(db$coverage[db$coverage >= breaks[2] & db$coverage <= max(c(cr, breaks))], plot=FALSE, breaks=(min(c(max(cr), max(breaks))) - breaks[2])/steps[3])$breaks)
-    s <- unique(s)
-    slab <- c(paste(s[1:length(s)-1], s[2:length(s)], sep="-"), paste(s[length(s)], "", sep="-"))
-    it <- cut(db$coverage, s, include.lowest=TRUE)
-    db$bin <- it
-    dotchart(table(db$titv, it)[1,]/table(db$titv,it)[2,], ylab="coverage", xlab="ti/tv", ...)
+## All diBayes related methods are put here
+setClass("diBayes", contains="RangedData", representation=representation(titv="character", funccode="factor", gene="character"))
+setClass("diBayes", contains="RangedData")
+
+## helper function to import gff to diBayes
+read.diBayes.gff <- function(file) {
+    gff <- import.gff(file)
+    ## dbSNP function codes: Locus region = 1; Coding = 2; Coding-synon
+    ## = 3; Coding-nonsynon = 4; mRNA-UTR = 5; Intron = 6; Splice-site
+    ## = 7; Contig-reference = 8; Coding-exception = 9; NearGene-3 =
+    ## 13; NearGene-5 = 15; Coding-nonsynonymous nonsense = 41;
+    ## Coding-nonsynonymous missense = 42; Coding-nonsynonymous
+    ## frameshift = 44; Coding-nonsynonymous cds-indel = 45; UTR-3 =
+    ## 53; UTR-5 = 55; Splice-3 = 73; Splice-3 = 75.
+    fc <- do.call("c", lapply(strsplit(gsub("[A-Za-z0-9\\-\\.]+[:\\-]", "", gff$functionCode), ","), function(x) { paste(as.vector(unique(sort(x))), collapse=",")}))
+    fc[fc==""] <- "NA"
+    gff$funccode <- factor(fc)
+    gff$titv <- factor(mapply(function(x,y) {.titv.df[x,y]}, gff$genotype, gff$reference), levels=c("ti", "tv"))
+    names(gff$titv) <- NULL
+    ## Dangerous: assuming gene names have no commas or :
+    gff$gene <- gsub(":[0-9]+$", "", gsub("^[0-9]+:", "", gsub(",.*", "", gff$functionCode)))
+    gff.db <- new("diBayes", gff)
+    gff.db
 }
 
-## Enable formula expression?
-plot.diBayes <- function(res, type="chr", genenames=NULL, cond=NULL, ...) {
-    type <- match.arg(type, c("chr", "gene"))
-    if (type == "chr") {
-        chrdata <- space(res)
-        i <- !grepl("[a-zA-Z]", levels(chrdata))
-        newl <- c(sort(as.integer(levels(chrdata)[i])), levels(chrdata)[!i])
-        chrdata <- factor(chrdata, levels=newl)
-        barchart(chrdata, xlab="Freq", ...)
-    }
-    else if (type == "gene") {
-        if (!is.null(genenames)) {
-            z <- do.call("c", lapply(genenames, function(x) {tmp <- grep(x, res$functionCode); y <- if(length(tmp)>1) length(tmp) else 0; names(y) <- x; y}))
-            barchart(z, xlab="Freq", ylab="Genes", ...)
-        } else {
-            stop("please provide gene names")
-        }
-    }
-}
+## filterVariants
+setGeneric("filterVariants", function(object, cov=c(0,Inf), score=c(0, 1), genenames=NULL, maf.freq=0, only.novel=FALSE) {standardGeneric("filterVariants")})
 
-summary.diBayes <-
-function(db, fc.return=FALSE) {
-    res <- list()
-    labs <- c("all", "hom", "het")
-    indbsnp <- factor(!is.na(db$rsID), levels=c(FALSE, TRUE))
-    db$het <- factor(db$het, levels=c(0,1))
-    dbsnp <- table(indbsnp, db$het)
-    res$dbsnp <- list()
-    res$dbsnp$conc <- c(prop.table(margin.table(dbsnp, 1))[2],
-                        prop.table(table(indbsnp, db$het),2)[2,])
-    names(res$dbsnp$conc) <- labs
-    res$dbsnp$count <- c(margin.table(dbsnp,1)[2],
-                         dbsnp[2,])
-    names(res$dbsnp$count) <- labs
-
-    res$basic <- c(sum(table(db$het)), table(db$het))
-    names(res$basic) <- labs
-
-    genotype <-  factor(db$genotype, levels=.iub)
-    res$snpchanges <- table(genotype, reference=factor(db$reference, levels=c("A", "C", "G", "T")) )
-    het <- rep(c(rep("hom", 4), rep("het", 6)), 4)
-    res$titv <- tapply(res$snpchanges, list(.titv, het), sum)
-    res$titv <- cbind(res$titv, all=margin.table(res$titv, 1))
-    ## Chromosome stats
-    chrdata <- space(db)
-    i <- !is.na(as.integer(levels(chrdata)))
-    newl <- c(sort(as.integer(levels(chrdata))[i]), levels(chrdata)[!i])
-    res$chrstats <- cbind(table(factor(chrdata, levels=newl)), table(factor(chrdata, levels=newl), db$het))
-    colnames(res$chrstats) <- c("all", "hom", "het")
-
-    if (fc.return) {
-        ## Summary based on functional code
-        i <- do.call("c", lapply(db$funccode, function(x) {y <- strsplit(as.character(x), ","); length(y[[1]])}))
-        res$fc <- list(fc = do.call("c", lapply(db$funccode, function(x) {y <- strsplit(as.character(x), ","); y[[1]]})),
-                       j = rep(1:length(i), i)
-                       )
-        class(res$fc) <- c("funccode", "list")
-    } else {
-        res$fc <- NULL
-    }
-    class(res) <- c("diBayes", "list")
-    res
-}
-
-plot.funccode <-
-function(fc, ...) {
-    funccodes <- list("Locus region", "Coding","Coding-synon","Coding-nonsynon",
-                      "mRNA-UTR", "Intron", "Splice-site",
-                      "Contig-reference", "Contig-exception",
-                      "NearGene-3", "NearGene-5",
-                      "Coding-nonsynonymous nonsense", "Coding-nonsynonymous missense",
-                      "Coding-nonsynonymous frameshift",
-                      "Coding-nonsynonymous cds-indel", "UTR-3", "UTR-5", "Splice-3",
-                      "Splice-5")
-    names(funccodes) <- as.character(c(1,2,3,4,5,6,7,8,9,13,15,41,42,44,45,53,55,73,75))
-    fctab <- table(fc$fc)
-    i <- match(names(fctab), names(funccodes))
-    labs <- do.call("rbind", funccodes[i[!is.na(i)]])
-    labs <- paste(rownames(labs), labs)
-    #if (length(labs) < length(names(fctab))) labs <- c(labs, NA)
-    barplot(fctab, horiz=TRUE, legend.text=labs, ...)
-}
-
-filter.diBayes <-
-function(db, cov=c(0,Inf), score=c(0, 1), genenames=NULL, ...) {
+setMethod("filterVariants",
+          signature=signature(
+          object="diBayes"),
+          function(object, cov=c(0,Inf), score=c(0, 1), genenames=NULL, maf.freq=0, only.novel=FALSE) {
     i.all <- rep(FALSE, dim(db)[1])
     i.cov <- db$coverage >= cov[1] & db$coverage <= cov[2]
     i.score <- db$score >= score[1] & db$score <= score[2]
+    i.maffreq <- db$novelAlleleCounts/(db$refAlleleCounts + db$novelAlleleCounts) > maf.freq
     i.names <- !i.all
     if (!is.null(genenames)) {
         i.names <- i.all
         i.names[do.call("c", lapply(genenames, grep, db$functionCode))] <- TRUE
     }
-    i <- i.cov & i.score & i.names
-    db[i,]
+    i <- i.cov & i.score & i.names & i.maffreq
+    if (only.novel)
+        i <- i & is.na(db$rsID)
+    new("diBayes", db[i,])
 }
+          )
 
+## plotQC
+setGeneric("plotQC", function(object, formula = ~., range=c(0, max(object$coverage)), stepsize=1, titv.ratio=TRUE, return.tab=FALSE, ...) {
+    standardGeneric("plotQC")})
+
+setMethod("plotQC",
+          signature=signature(
+          object="diBayes"),
+          function(object, formula = ~., range=c(0,max(object$coverage)), stepsize=1, titv.ratio=TRUE, return.tab=FALSE, ...) {
+              ylab = "Freq"
+              db = object[object$coverage >= min(range) & object$coverage <= max(range),]
+              steps <- seq(min(range), max(range), by=stepsize)
+              db$intervals <- cut(db$coverage, steps, include.lowest=TRUE)
+              term.labels <- labels(terms.formula(formula))
+              term.labels <- gsub("\\|", "+", term.labels)
+              if (titv.ratio) {
+                  term.labels <- gsub("[+ ]*titv", "", term.labels)
+                  tab.df <- as.data.frame(xtabs(as.formula(paste("~ ", term.labels, "+ titv", sep="")), data=as.data.frame(db)))
+                  n.row <- dim(tab.df)[1]/2
+                  i.col <- match(c("titv", "Freq"), colnames(tab.df))
+                  c.names <- colnames(tab.df[-i.col])
+                  tab.df <- as.data.frame(cbind(tab.df[1:n.row,-i.col], Freq = tab.df$Freq[tab.df$titv=="ti"] / tab.df$Freq[tab.df$titv=="tv"]))
+                  colnames(tab.df)[1:length(c.names)] <- c.names
+                  newf <- as.formula(paste("Freq ~ ", gsub("\\|[ ]*titv", "", labels(terms.formula(formula)))))
+                  ylab = "titv ratio"
+              } else {
+                  tab.df <- as.data.frame(xtabs(as.formula(paste("~ ", term.labels, sep="")), data=as.data.frame(db)))
+                  newf <- as.formula(paste("Freq ~ ", labels(terms.formula(formula))))
+              }
+              if (return.tab)
+                  tab.df
+              else
+                  xyplot(newf, data=tab.df, ylab=ylab, ...)
+          }
+          )
+
+setGeneric("Barchart", function(object, formula, ...) {standardGeneric("Barchart")})
+
+setMethod("Barchart",
+          signature=signature(
+          object="diBayes"),
+          function(object, formula, ...) {
+              tab.df <- xtabs(formula, data=object)
+              barchart(tab.df, ...)
+          }
+          )
+
+setMethod("summary",
+          signature=signature(
+          object="diBayes"),
+          function(object) {
+              res <- list()
+              labs <- c("all", "hom", "het")
+              indbsnp <- factor(!is.na(object$rsID), levels=c(FALSE, TRUE))
+              object$het <- factor(object$het, levels=c(0,1))
+              dbsnp <- table(indbsnp, object$het)
+              res$dbsnp <- list()
+              res$dbsnp$conc <- c(prop.table(margin.table(dbsnp, 1))[2],
+                                  prop.table(table(indbsnp, object$het),2)[2,])
+              names(res$dbsnp$conc) <- labs
+              res$dbsnp$count <- c(margin.table(dbsnp,1)[2],
+                                   dbsnp[2,])
+              names(res$dbsnp$count) <- labs
+              res$basic <- c(sum(table(object$het)), table(object$het))
+              names(res$basic) <- labs
+                            genotype <-  factor(object$genotype, levels=.iub)
+              res$snpchanges <- table(genotype, reference=factor(object$reference, levels=c("A", "C", "G", "T")) )
+              het <- rep(c(rep("hom", 4), rep("het", 6)), 4)
+              res$titv <- tapply(res$snpchanges, list(.titv, het), sum)
+              res$titv <- cbind(res$titv, all=margin.table(res$titv, 1))
+              ## Chromosome stats
+              res$chrstats <- xtabs(~ space + het, data=as.data.frame(object))
+              res$chrstats <- cbind(res$chrstats, all=margin.table(res$chrstats, 1))
+              res
+          }
+          )
+
+setGeneric("funcCodes", function(object) {standardGeneric("funcCodes")})
+
+setMethod("funcCodes",
+          signature=signature(
+          object="diBayes"),
+          function(object) {
+              i <- do.call("c", lapply(object$funccode, function(x) {y <- strsplit(as.character(x), ","); length(y[[1]])}))
+              fc <- list(fc=do.call("c", lapply(object$funccode, function(x) {y <- strsplit(as.character(x), ","); y[[1]]})),
+                         j = rep(1:length(i), i)
+                         )
+              fc
+          }
+          )
+
+setGeneric("plotFunctionalCode", function(object, ...) {standardGeneric("plotFunctionalCode")})
+
+setMethod("plotFunctionalCode",
+          signature=signature(
+          object="diBayes"),
+          function(object, ...) {
+              funccodes <- list("Locus region", "Coding","Coding-synon","Coding-nonsynon",
+                                "mRNA-UTR", "Intron", "Splice-site",
+                                "Contig-reference", "Contig-exception",
+                                "NearGene-3", "NearGene-5",
+                                "Coding-nonsynonymous nonsense", "Coding-nonsynonymous missense",
+                                "Coding-nonsynonymous frameshift",
+                                "Coding-nonsynonymous cds-indel", "UTR-3", "UTR-5", "Splice-3",
+                                "Splice-5")
+              names(funccodes) <- as.character(c(1,2,3,4,5,6,7,8,9,13,15,41,42,44,45,53,55,73,75))
+              fc <- funcCodes(object)
+              fc.tab <- table(fc$fc)
+              i <- match(names(fc.tab), names(funccodes))
+              labs <- do.call("rbind", funccodes[i[!is.na(i)]])
+              labs <- paste(rownames(labs), labs)
+              barplot(fc.tab, horiz=TRUE, legend.text=labs, ...)
+          }
+          )
