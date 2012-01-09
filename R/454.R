25ed441d98142ad71ee5bab0fb2eb44e6222ff1c
Per Unneberg <per.unneberg@scilifelab.se>
2012-01-09 17:04:36 +0100

    Add 454 io functionality

---
+++ R/454.R
@@ -0,0 +1,15 @@
+read454.metrics <- function(indir) {
+    infile <- file.path(indir, "454AllControlMetrics.csv")
+    lines <- gsub("\"", "", readLines(infile))
+    i <- grep("^Key", lines)
+    res <- list()
+    res$region1 <- list()
+    res$region2 <- list()
+    res$region1$data <- read.csv(textConnection(lines[(i[1]):(i[1]+2)]))
+    res$region2$data <- read.csv(textConnection(lines[(i[2]):(i[2]+2)]))
+    i <- grep("^Accuracy", lines )
+    j <- grep("^region", lines )
+    res$region1$accuracy <- read.csv(textConnection(lines[(i[1]):(j[2]-2)]))
+    res$region2$accuracy <- read.csv(textConnection(lines[(i[2]):length(lines)]))
+    return(res)
+}
